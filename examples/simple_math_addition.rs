282d27164397b9e6665dca05d5aae8388395878e
dudoof <email@example.com>
2023-07-11 14:12:25 +0200

    fix

--- examples/simple_math_addition.rs
+++ examples/simple_math_addition.rs
@@ -3,22 +3,13 @@ pub fn main() {
     let a = 1 + 1;
     println!("result of a ; {}", a);
 
-    let s = 2 - 1;
-    println!("result of s ; {}", s);
-    
-    // inside method
+    // inside function
     let _a: i32 = 2;
     let _b: i32 = 1;
     let _sum_addition: i32 = addition(_a, _b);
     println!("result of function addition    => {}", _sum_addition);
-    let _difference_subtraction = subtraction(_a, _b);
-    println!("result of function subtraction => {}", _difference_subtraction);
 }
 
 fn addition(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-fn subtraction(a: i32, b: i32) -> i32 {
     a + b
 }
---
+++ examples/simple_math_subtraction.rs
@@ -0,0 +1,17 @@
+pub fn main(){
+
+    let s = 2 - 1;
+    println!("result of s ; {}", s);
+
+
+    // inside function
+    let _a: i32 = 2;
+    let _b: i32 = 1;
+    let _difference_subtraction = subtraction(_a, _b);
+    println!("result of function subtraction => {}", _difference_subtraction);
+
+}
+
+fn subtraction(a: i32, b: i32) -> i32 {
+    a + b
+}
